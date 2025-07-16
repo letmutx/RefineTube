9bceb228ca6594219b4be66aef554dfc5cfa82f7
Rohit Kumar <letmutx@icloud.com>
2025-07-16 13:36:55 +0000

    Add name and description

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     manifest: {
+        name: "RefineTube",
+        description: "RefineTube is a browser extension that helps control your YouTube usage by providing AI-powered video ratings.",
         permissions: ["scripting", "storage"],
         host_permissions: ["*://i.ytimg.com/*"]
     },
